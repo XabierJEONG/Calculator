3a6a10ec66c9bd89be16347fb943f00afbc0b497
Robbie <robbie331@>
2024-07-24 13:50:59 +0900

    Level 1 사칙연산, 결과값출력 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,5 +12,35 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char oper = sc.next().charAt(0);
+
+        int result = 0;
+
+        if (oper == '+') {
+
+            result = num1 + num2;
+
+        } else if (oper == '-') {
+
+            result = num1 - num2;
+
+        } else if (oper == '*') {
+
+            result = num1 * num2;
+
+        } else if (oper == '/') {
+
+            result = num1 / num2;
+
+        } else {
+            System.out.println("유효하지 않은 연산자 입니다.");
+        }
+
+        System.out.println("결과: " + result);
+
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+
     }
 }

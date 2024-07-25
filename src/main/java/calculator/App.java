24fe1a2fbf84acd7a642373ce5848f68c04aba80
Robbie <robbie331@>
2024-07-25 11:14:54 +0900

    Level 1-8 결과값조회추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,14 +8,13 @@ public class App {
 
     public static void main(String[] args) {
         ArrayList<Double> resultArray = new ArrayList<>();
-
+        Scanner sc = new Scanner(System.in);
 //        double[] resultArray = new double[10];
 //        double[] newResultArray = new double[resultArray.length - 1];
 //        int index = 0;
 //        int indexRemove = 0;
 
-        Scanner sc = new Scanner(System.in);
-
+        // 계산 반복
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -60,16 +59,26 @@ public class App {
 //                }
 //            }
 
-            //
+            // 결과 값 저장.
             resultArray.add(result);
 
+            // 연산결과 삭제여부.
             System.out.print("가장 먼저 저장된 연산결과를 삭제하시겠습니까? (remove 입력) ");
             String remove = sc.next();
-            if (remove.equals("remove")){
+            if (remove.equals("remove")) {
                 resultArray.remove(0);
             }
 
+            //연산결과 조회.
+            System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
+            String inquiry = sc.next();
+            if (inquiry.equals("inquiry")) {
+                for (double resultArrays: resultArray) {
+                    System.out.println(resultArrays);
+                }
+            }
 
+            // 추가 계산여부.
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
             String exit = sc.next();
             if (exit.equals("exit")) {
@@ -78,7 +87,7 @@ public class App {
         }
 
 //        System.out.println("배열 저장확인: " + Arrays.toString(resultArray));
-
-        System.out.println("컬렉션 저장확인: " + resultArray.toString());
+//
+//        System.out.println("컬렉션 저장확인: " + resultArray.toString());
     }
 }

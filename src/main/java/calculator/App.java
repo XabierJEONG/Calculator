c16a464c59b879769f9082679b1484fc6175c279
Robbie <robbie331@>
2024-07-30 18:51:30 +0900

    Level 2-7 원넓이 계산 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,47 +7,74 @@ public class App {
 
     public static void main(String[] args) {
         ArrayList<Double> resultArray = new ArrayList<>();
-        Calculator calculator = new Calculator(resultArray);
+        ArrayList<Double> pieResultArray = new ArrayList<>();
+        Calculator calculator = new Calculator(resultArray, pieResultArray);
         Scanner sc = new Scanner(System.in);
 
         // 숫자, 연산자 입력
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            System.out.print("사칙연산을 원하시면 '0'을, 원의 너비를 원하시면 '1'을 입력하세요 : ");
+            int num0 = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char oper = sc.next().charAt(0);
+            // 사칙연산
+            if (num0 == 0) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
 
-            try {
-                double result = calculator.calculate(oper, num1, num2);
-                System.out.println("결과 : " + calculator.getResult());
-            } catch (CalException e) {
-                System.out.println("에러 : " + e.getMessage());
-            }
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char oper = sc.next().charAt(0);
 
-//            //set 메소드
-//            calculator.setResult(3);
-//            System.out.println(calculator.getResult());
+                try {
+                    double result = calculator.calculate(oper, num1, num2);
+                    System.out.println("결과 : " + calculator.getResult());
+                } catch (CalException e) {
+                    System.out.println("에러 : " + e.getMessage());
+                }
 
-            // 연산결과 삭제여부.
-            System.out.print("가장 먼저 저장된 연산결과를 삭제하시겠습니까? (remove 입력) ");
-            String remove = sc.next();
-            calculator.removeResult(remove);
+                // 연산결과 삭제여부.
+                System.out.print("가장 먼저 저장된 연산결과를 삭제하시겠습니까? (remove 입력) ");
+                String remove = sc.next();
+                calculator.removeResult(remove);
 
+                //연산결과 조회.
+                System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
+                String inquiry = sc.next();
+                calculator.inquiryResults(inquiry);
 
-            //연산결과 조회.
-            System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
-            String inquiry = sc.next();
-            calculator.inquiryResults(inquiry);
+                // 추가 계산여부.
+                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
+                String exit = sc.next();
+                if (exit.equals("exit")) {
+                    break;
+                }
+            // 원넓이 계산
+            } else if (num0 == 1) {
+                System.out.print("원의 반지름을 입력하세요: ");
+                int radNum1 = sc.nextInt();
+                try {
+                    double pieResult = calculator.pieCalculate(radNum1);
+                    System.out.println("결과 : " + calculator.getPieResult());
+                } catch (CalException e) {
+                    System.out.println("에러: " + e.getMessage() + "0은 입력할 수 없습니다");
+                }
+                // 연산결과 삭제여부.
+                System.out.print("가장 먼저 저장된 연산결과를 삭제하시겠습니까? (remove 입력) ");
+                String remove = sc.next();
+                calculator.removePieResult(remove);
 
-            // 추가 계산여부.
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
-            String exit = sc.next();
-            if (exit.equals("exit")) {
-                break;
+                //연산결과 조회.
+                System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
+                String inquiry = sc.next();
+                calculator.inquiryPieResults(inquiry);
 
+                // 추가 계산여부.
+                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) ");
+                String exit = sc.next();
+                if (exit.equals("exit")) {
+                    break;
+                }
             }
         } sc.close();
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,16 +3,53 @@ package calculator;
 import java.util.ArrayList;
 
 public class Calculator {
+
+    // 필드
     private ArrayList<Double> resultArray;
+    private ArrayList<Double> pieResultArray;
     private double result;
-    public Calculator(ArrayList<Double> resultArray) {
+    private double pieResult;
+    final double PI = 3.14; // 원주율은 변하지 않는 상수이기에 fianl 사용.
+
+    // 생성자
+    public Calculator(ArrayList<Double> resultArray, ArrayList<Double> pieResultArray) {
         this.resultArray = resultArray;
+        this.pieResultArray = pieResultArray;
     }
-    // 계산기 메소드
-    public double calculate(char oper, int num1, int num2) throws CalException{
+
+    // 원 넓이 계산기 메소드
+    public double pieCalculate(int radNum1) throws CalException {
+        if (radNum1 != 0) {
+            pieResult = radNum1 * radNum1 * PI;
+        }
+        pieResultArray.add(pieResult);
+        return pieResult;
+    }
+
+    // 원 넓이 getter 메소드
+    public double getPieResult() {
+        return pieResult;
+    }
+    // 원 넓이 setter 메소드
+    public void setPieResult(double pieResult) {
+        this.pieResult = pieResult;
+    }
+    // 원 넓이 삭제 메소드
+    public void removePieResult(String remove) {
+        if (remove.equals("remove")) {
+            pieResultArray.remove(0);
+        }
+    }
+    // 원 넓이 컬렉션 필드의 저장된 결과값 조회
+    public void inquiryPieResults(String inquiry) {
+        if (inquiry.equals("inquiry")) {
+            System.out.println(pieResultArray);
+        }
+    }
 
 
-
+    // 사칙연산계산기 메소드
+    public double calculate(char oper, int num1, int num2) throws CalException {
         if (oper == '+') {
             result = num1 + num2;
         } else if (oper == '-') {
@@ -31,27 +68,21 @@ public class Calculator {
         return result;
     }
 
-    // getter 메소드
+    // 사칙연산 getter 메소드
     public double getResult() {
         return result;
     }
-    // setter 메소드
+    // 사칙연산 setter 메소드
     public void setResult(double result) {
         this.result = result;
     }
-
-//    값 확인
-//    public ArrayList<Double> getResultArray() {
-//        return resultArray;
-//    }
-
-    //삭제 메소드
+    // 사칙연산 삭제 메소드
     public void removeResult(String remove) {
         if (remove.equals("remove")) {
             resultArray.remove(0);
         }
     }
-
+    // 사칙연산 컬렉션 필드의 저장된 결과값 조회
     public void inquiryResults(String inquiry) {
         if (inquiry.equals("inquiry")) {
             System.out.println(resultArray);
